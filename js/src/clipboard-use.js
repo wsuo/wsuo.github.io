5d89b109a315ff0e21547090650ba17ddcec548e
wsuo <ws2821@yeah.net>
2020-04-03 21:52:12 +0800

    Site updated: 2020-04-03 21:52:01

--- js/src/clipboard-use.js
+++ js/src/clipboard-use.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 11 2020 15:34:47 GMT+0800 (GMT+08:00)
+// build time:Fri Apr 03 2020 21:51:59 GMT+0800 (GMT+08:00)
 !function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-clipboard"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
 //rebuild by neat 
